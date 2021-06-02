9a07b4cd4a0aa104ca2050205ec339169c0608eb
mtwatso2 <35612911+mtwatso2@users.noreply.github.com>
2021-06-02 13:08:15 -0500

    Update 6/2/21

--- app.R
+++ app.R
@@ -13,17 +13,17 @@ library(plotly)
 library(dplyr)
 library(reshape2)
 
-cfpb <- read.csv("panel1-9-28.csv") 
+cfpb <- read.csv("panel1-6-2-21.csv") 
 cfpb$Year <- factor(cfpb$Year)
 cfpb[cfpb == 0] <- NA
 
-mbData <- read.csv("panel2-9-28.csv")
+mbData <- read.csv("panel2-6-2-21.csv")
 mbData$Year <- factor(mbData$Year)
 
 mbData_melt <- melt(mbData, id = c("State", "Year", "Company"))
 mbData_melt <- mbData_melt[!(mbData_melt$variable == "Total"),]
 
-newComp <- read.csv('map2-9-28.csv')
+newComp <- read.csv('map2-6-2-21.csv')
 newComp$Year <- factor(newComp$Year)
 
 # Define UI for application that displays plots
@@ -31,7 +31,7 @@ ui <- fluidPage(
   
   tabsetPanel(
     
-    tabPanel("Complaint Counts", "CFPB Database: Data was downloaded on September 28, 2020",  
+    tabPanel("Complaint Counts", "CFPB Database: Data was downloaded on June 2, 2021",  
              helpText(a("Click here to see LDA Visualization", href = "https://mwatson717.shinyapps.io/CFPB_LDA/")),
              
              titlePanel( "Complaint Counts"),
